7c253984c7b755ef5c9a11e0ec4d2b859fe3fc02
Marian Domanski <mdomanski@usgs.gov>
2020-02-06 13:39:15 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+try:
+    from sphinx.setup_command import BuildDoc
+    sphinx_imported = True
+except BaseException:
+    sphinx_imported = False
+
+name = 'dynrat'
+version = '0.0'
+release = '0.0.1'
+dev_status = 'Development Status :: 1 - Planning'
+
+install_requires = ['numpy']
+
+setup_kwargs = {
+    'name': name,
+    'version': release,
+    'packages': ['dynrat'],
+    'url': 'https://code.usgs.gov/dynamic-rating/dynrat',
+    'license': 'License :: Public Domain',
+    'author': 'Marian Domanski',
+    'author_email': 'mdomanski@usgs.gov',
+    'description': 'Dynamic ratings',
+    'classifiers': [
+        dev_status,
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python 3.7'
+    ],
+    'python_requires': '~=3.7',
+    'install_requires': install_requires
+}
+
+if sphinx_imported:
+    cmdclass = {'build_sphinx': BuildDoc}
+    docs_source = 'docs/'
+    docs_build_dir = 'docs/_build'
+    docs_builder = 'html'
+    setup_kwargs['command_options'] = {'build_sphinx': {
+        'project': ('setup.py', name),
+        'version': ('setup.py', version),
+        'release': ('setup.py', release),
+        'source_dir': ('setup.py', docs_source),
+        'build_dir': ('setup.py', docs_build_dir),
+        'builder': ('setup.py', docs_builder)}}
+
+setup(**setup_kwargs)
