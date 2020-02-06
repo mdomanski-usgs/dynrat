653114b91a95445c2489f6c5cdd6e0e8cf30251b
Marian Domanski <mdomanski@usgs.gov>
2020-02-06 13:55:14 -0600

    implementing QSolver

--- dynrat/dynmod.py
+++ dynrat/dynmod.py
@@ -1,3 +1,14 @@
+"""Python implementation of DYNMOD
+
+References
+----------
+.. [1] Fread, D.L., 1973, A dynamic model of stage-discharge
+   relations affected by changing discharge: NOAA Technical
+   Memorandum NWS HYDRO-16, 55 p.
+   http://repository.library.noaa.gov/view/noaa/13480
+
+"""
+
 import numpy as np
 
 
@@ -63,21 +74,33 @@ class Frict:
 
 class Sect:
 
-    def __init__(self):
+    def area(self, stage):
+        pass
 
+    def top_width(self, stage):
         pass
 
 
 class QSolve:
+    """Iterative solver for discharge using stage time series.
 
-    def __init__(
-            self,
-            sect,
-            frict,
-            bed_slope,
-            slope_ratio,
-            time_step,
-            epsilon):
+    Parameters
+    ----------
+    sect : Sect
+        Provides cross section area and top width as a function of
+        stage.
+    frict : Frict
+        Provides Manning's n as a function of stage.
+    bed_slope : float
+        Bed slope of channel
+    slope_ratio : float
+        Ratio of the channel bed slope to an average wave slope.
+    time_step : float
+        Time series time step, in seconds.
+
+    """
+
+    def __init__(self, sect, frict, bed_slope, slope_ratio, time_step):
 
         self._bed_slope = bed_slope
         self._frict = frict
@@ -86,22 +109,34 @@ class QSolve:
         self._time_step = time_step
 
     def _dh(self, h, h_prime):
-        return (h - h_prime)/self._time_step
+        return (h - h_prime) / self._time_step
 
     @classmethod
     def _f(cls, q, l2, l3, l4, l5, l6):
+        """Zero function.
+
+        This private method is meant to be used internally during the
+        iterative solution of q. `l2`, `l3`, `l4`, `l5`, and `l6` do
+        not need to be computed for every iteration.
+        """
 
         l0 = cls._L0(q, l3, l4, l5, l6)
 
-        return q - l2*l0**(1/2)
+        return q - l2 * l0**(1 / 2)
 
     @classmethod
     def _f_prime(cls, q, l2, l3, l4, l5, l6):
+        """Derivative (with respect to q) of the zero function.
+
+        This private method is meant to be used internally during the
+        iterative solution of q. `l2`, `l3`, `l4`, `l5`, and `l6` do
+        not need to be computed for every iteration.
+        """
 
         l0 = cls._L0(q, l3, l4, l5, l6)
         l1 = cls._L1(q, l4, l5, l6)
 
-        return 1 - 0.5*l2*l1/l0**(1/2)
+        return 1 - 0.5 * l2 * l1 / l0**(1 / 2)
 
     def _K(self, h, h_prime):
 
@@ -111,17 +146,17 @@ class QSolve:
 
         dBdh = (top_width - top_width_prime) / (h - h_prime)
 
-        return 5/3 - 2/3*(area/top_width**2)*dBdh
+        return 5 / 3 - 2 / 3 * (area / top_width**2) * dBdh
 
     @staticmethod
     def _L0(q, l3, l4, l5, l6):
 
-        return l3 + l4/q + l5*q + l6*q**2
+        return l3 + l4 / q + l5 * q + l6 * q**2
 
     @staticmethod
     def _L1(q, l4, l5, l6):
 
-        return -l4/q**2 + l5 + 2*l6*q
+        return -l4 / q**2 + l5 + 2 * l6 * q
 
     def _L2(self, h):
 
@@ -129,16 +164,16 @@ class QSolve:
         top_width = self._sect.top_width(h)
         n = self._frict.roughness(h)
 
-        hydraulic_depth = area/top_width
+        hydraulic_depth = area / top_width
 
-        return 1.486*area*hydraulic_depth**(2/3)/n
+        return 1.486 * area * hydraulic_depth**(2 / 3) / n
 
     def _L3(self, q_prime, h_prime):
 
         area_prime = self._sect.area(h_prime)
 
-        return self._bed_slope + 2/3 * self._bed_slope/self._slope_ratio**2 \
-            + q_prime/(GRAVITY * area_prime * self._time_step)
+        return self._bed_slope + 2 / 3 * self._bed_slope / self._slope_ratio**2 \
+            + q_prime / (GRAVITY * area_prime * self._time_step)
 
     def _L4(self, h, h_prime):
 
@@ -154,18 +189,34 @@ class QSolve:
         area = self._sect.area(h)
         dh = self._dh(h, h_prime)
 
-        return (1 - 1/k)*top_width*dh/(GRAVITY*area**2) \
-            - 1/(GRAVITY*area*self._time_step)
+        return (1 - 1 / k) * top_width * dh / (GRAVITY * area**2) \
+            - 1 / (GRAVITY * area * self._time_step)
 
     def _L6(self, h):
 
         top_width = self._sect.top_width(h)
         area = self._sect.area(h)
 
-        return -2/3*(self._bed_slope*top_width) \
-            / (self._slope_ratio**2*GRAVITY*area**3)
+        return -2 / 3 * (self._bed_slope * top_width) \
+            / (self._slope_ratio**2 * GRAVITY * area**3)
 
     def zero_func(self, h, h_prime, q, q_prime):
+        """Iterative solution zero function
+
+        Computes the zero function (eq. 15 of [1]_).
+
+        Parameters
+        ----------
+        h : float
+            Stage of current time step
+        h_prime : float
+            Stage of previous time step
+        q : float
+            Discharge of current time step
+        q_prime : float
+            Discharge of previous time step
+
+        """
 
         l2 = self._L2(h)
         l3 = self._L3(q_prime, h_prime)
