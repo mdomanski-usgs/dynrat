a2a2f6e0b6618a6f5951fda06f7e06d44e4d3e7e
Marian Domanski <mdomanski@usgs.gov>
2020-08-06 15:41:29 -0500

    add TimeSeries and ContinuousTimeSeries

--- dynrat/timeseries.py
+++ dynrat/timeseries.py
@@ -1,4 +1,99 @@
 import numpy as np
+import pandas as pd
+
+
+class TimeSeries:
+    """Time series class
+
+    Parameters
+    ----------
+    data : pandas.Series
+
+    """
+
+    def __init__(self, data):
+
+        self._data = data.copy(deep=True)
+
+    def data(self):
+        """Returns a copy of the data contained in this
+        time series
+
+        Returns
+        -------
+        pandas.Series
+
+        """
+
+        return self._data.copy()
+
+    @classmethod
+    def from_aq_csv(cls, csv_path):
+        """Reads time series from a CSV file obtained from
+        Aquarius
+
+        Parameters
+        ----------
+        csv_path : str
+            Path to CSV file
+
+        Returns
+        -------
+        TimeSeries
+
+        """
+
+        df = pd.read_csv(csv_path, index_col=0, parse_dates=True)
+
+        ts = df['Value']
+        ts.index.name = 'DateTime'
+
+        return cls(ts)
+
+    def values(self):
+        """Returns an array of observed values in this time
+        series
+
+        Returns
+        -------
+        numpy.ndarray
+
+        """
+
+        return self._data.values
+
+
+class ContinuousTimeSeries(TimeSeries):
+
+    def change_freq(self, freq, interp_missing=False):
+        """Change the frequency of this time series
+
+        Parameters
+        ----------
+        time_step : float
+            New time step, in seconds
+        interp_missing : boolean, optional
+            Interpolate missing values in the new time
+            series
+
+        Returns
+        -------
+        ContinuousTimeSeries
+
+        """
+
+        freq = pd.tseries.offsets.DateOffset(seconds=freq)
+        index = pd.date_range(
+            start=self._data.index[0], end=self._data.index[-1], freq=freq,
+            tz=self._data.index.tz)
+        index.name = 'DateTime'
+
+        data = self._data[index]
+
+        if interp_missing:
+            data = data.interpolate()
+
+        return self.__class__(data)
 
 
 class QTimeSeries:
