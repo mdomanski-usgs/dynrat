98799739f38f6eb2d5f97bb3ffa893f5b4e91075
Marian Domanski <mdomanski@usgs.gov>
2020-08-12 11:03:59 -0500

    add plot module

---
+++ dynrat/plot.py
@@ -0,0 +1,12 @@
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+
+
+def time_series_axes(ax=None):
+
+    if ax is None:
+        ax = plt.axes()
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+
+    return ax
