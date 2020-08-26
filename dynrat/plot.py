03f5c145d94a4e763bf87ef8832c8b4b97039006
Marian Domanski <mdomanski@usgs.gov>
2020-08-26 12:57:31 -0500

    add docstring and label kwarg

--- dynrat/plot.py
+++ dynrat/plot.py
@@ -3,7 +3,29 @@ import matplotlib.pyplot as plt
 from dynrat.timeseries import ContinuousTimeSeries, TimeSeries
 
 
-def stage_discharge_plot(stage, discharge, ax=None):
+def stage_discharge_plot(stage, discharge, ax=None, label=None):
+    """Plot stage vs. discharge
+
+    Parameters
+    ----------
+    stage : TimeSeries
+        Stage time series
+    discharge : TimeSeries
+        Discharge time series
+    ax : matplotlib.axes.Axes, optional
+        If None, a new axes is created and returned.
+    label : str, optional
+        Label for the line. If None and stage and discharge
+        are :class:`.ContinuousTimeSeries`, ``'WSC Computed
+        Discharge'`` is used. If None and stage and
+        discharge are instances of a :class:`.TimeSeries`
+        subclass, ``'Observed Discrete Discharge'`` is used.
+
+    Return
+    ------
+    matplotlib.axes.Axes
+
+    """
 
     if ax is None:
         ax = plt.axes()
@@ -11,19 +33,25 @@ def stage_discharge_plot(stage, discharge, ax=None):
     if isinstance(stage, ContinuousTimeSeries) and \
             isinstance(discharge, ContinuousTimeSeries):
 
+        if label is None:
+            label = 'WSC Computed Discharge'
+
         h_series = stage.data()
         q_series = discharge.data()
 
         inter_idx = h_series.index.intersection(q_series.index)
 
         ax.plot(q_series[inter_idx].values, h_series[inter_idx].values,
-                label='WSC Computed Discharge', linestyle='solid',
+                label=label, linestyle='solid',
                 color='darkslategray')
 
     elif isinstance(stage, TimeSeries) and isinstance(discharge, TimeSeries):
 
+        if label is None:
+            label = 'Observed Discrete Discharge'
+
         ax.scatter(discharge.values(), stage.values(),
-                   label='Observed Discrete Discharge', color='darkorchid')
+                   label=label, color='darkorchid')
 
     else:
         raise TypeError("Unrecognized types of time series")
