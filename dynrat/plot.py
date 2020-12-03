5ae343b0f441f0280dc099f7bf8df34d04eb313a
Marian Domanski <mdomanski@usgs.gov>
2020-12-03 16:10:17 -0600

    add discharge hydrograph to multicolor_h_vs_q

--- dynrat/plot.py
+++ dynrat/plot.py
@@ -6,44 +6,63 @@ import numpy as np
 from dynrat.timeseries import ContinuousTimeSeries, TimeSeries
 
 
-def multicolor_h_vs_q(stage, discharge, ax=None, colormap='binary'):
+def multicolor_h_vs_q(stage, discharge, axs=None, colormap='binary'):
 
-    if ax is None:
-        ax = plt.axes()
+    if axs is None:
+        fig = plt.figure()
+        ax1 = plt.subplot2grid((4, 1), (0, 0), colspan=1, rowspan=1, fig=fig)
+        ax2 = plt.subplot2grid((4, 1), (1, 0), colspan=1, rowspan=3, fig=fig)
 
     h_series = stage.data()
     q_series = discharge.data()
 
     inter_idx = h_series.index.intersection(q_series.index)
 
-    x = q_series[inter_idx].values
-    y = h_series[inter_idx].values
+    q_values = q_series[inter_idx].values
+    h_values = h_series[inter_idx].values
     time_as_float = inter_idx.values.astype(float)
-    norm_time_as_float = (time_as_float - time_as_float.min()) / \
-        (time_as_float.max() - time_as_float.min())
-
-    points = np.array([x, y]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]], axis=1)
-
+    time_range = time_as_float.max() - time_as_float.min()
+    norm_time_as_float = (time_as_float - time_as_float.min()) / time_range
     norm = plt.Normalize(norm_time_as_float.min(), norm_time_as_float.max())
-    lc = LineCollection(segments, cmap=colormap, norm=norm)
 
-    lc.set_array(norm_time_as_float)
+    # discharge vs time hydrograph
+    q_t_points = np.array([norm_time_as_float, q_values]).T.reshape(-1, 1, 2)
+    q_t_segments = np.concatenate([q_t_points[:-1], q_t_points[1:]], axis=1)
+    q_t_lc = LineCollection(q_t_segments, cmap=colormap, norm=norm)
+    q_t_lc.set_array(norm_time_as_float)
+    q_t_lc.set_linewidth(2)
+    ax1.add_collection(q_t_lc)
 
-    lc.set_linewidth(2)
-    ax.add_collection(lc)
+    # stage vs discharge plot
+    h_q_points = np.array([q_values, h_values]).T.reshape(-1, 1, 2)
+    h_q_segments = np.concatenate([h_q_points[:-1], h_q_points[1:]], axis=1)
+    h_q_lc = LineCollection(h_q_segments, cmap=colormap, norm=norm)
+    h_q_lc.set_array(norm_time_as_float)
+    h_q_lc.set_linewidth(2)
+    ax2.add_collection(h_q_lc)
+
+    h_range = h_values.max() - h_values.min()
+    q_range = q_values.max() - q_values.min()
 
     margin = 0.1
 
-    ax.set_xlabel('Discharge, in cfs')
-    x_range = x.max() - x.min()
-    ax.set_xlim(x.min() - margin*x_range, x.max() + margin*x_range)
+    q_lim = (q_values.min() - margin*q_range,
+             q_values.max() + margin*q_range)
 
-    ax.set_ylabel('Stage, in ft')
-    y_range = y.max() - y.min()
-    ax.set_ylim(y.min() - margin*y_range, y.max() + margin*y_range)
+    h_lim = (h_values.min() - margin*h_range,
+             h_values.max() + margin*h_range)
 
-    return ax
+    ax1.set_ylabel('Discharge, in cfs')
+    ax1.tick_params(axis='x', which='both', bottom=False,
+                    top=False, labelbottom=False)
+    ax1.set_ylim(q_lim)
+
+    ax2.set_xlabel('Discharge, in cfs')
+    ax2.set_xlim(q_lim)
+    ax2.set_ylabel('Stage, in ft')
+    ax2.set_ylim(h_lim)
+
+    return fig
 
 
 def stage_discharge_plot(stage, discharge, ax=None, color=None, label=None):
