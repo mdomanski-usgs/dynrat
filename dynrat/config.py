b96ec14820fc62973e2b8f23461ba891354a6876
Marian Domanski <mdomanski@usgs.gov>
2021-10-19 11:57:46 -0500

    implement configuration class

---
+++ dynrat/config.py
@@ -0,0 +1,34 @@
+import os
+
+import yaml
+
+
+class Config:
+
+    def __init__(self, path):
+
+        if not os.path.exists(path):
+            raise File
+        self._config_path = path
+
+        self._load_config()
+
+    def _load_config(self):
+
+        with open(self._config_path, 'r') as stream:
+            config = yaml.load(stream, yaml.Loader)
+
+        self._config = config
+
+    def __getitem__(self, key):
+
+        self._load_config()
+
+        if key in self._config.keys():
+            return self._config[key]
+        else:
+            raise KeyError
+
+    def __repr__(self):
+
+        return self._config.__repr__()
